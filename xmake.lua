cee1e762d350c54e49ebf35a0dc768298b09297c
n15421 <119112174+n15421@users.noreply.github.com>
2025-02-05 20:51:59 +0800

    chore:update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -3,49 +3,36 @@ add_rules("mode.debug", "mode.release", "mode.releasedbg")
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 add_repositories("groupmountain-repo https://github.com/GroupMountain/xmake-repo.git")
 
+add_requires("levilaminalibrary")
+add_requires("gmlib")
+add_requires("levibuildscript")
+
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-add_requires("levilamina")
-add_requires("levibuildscript")
-add_requires("gmlib")
+option("target_type")
+    set_default("server")
+    set_showmenu(true)
+    set_values("server", "client")
+option_end()
 
 target("InventoryCheck") -- Change this to your mod name.
-    add_cxflags(
-        "/EHa",
-        "/utf-8"
-    )
-    add_files(
-        "src/**.cpp"
-    )
-    add_includedirs(
-        "src"
-    )
-    add_packages(
-        "levilamina",
-        "gmlib"
-    )
-    add_defines(
-        "NOMINMAX", 
-        "UNICODE", 
-        "_HAS_CXX17",
-        "_HAS_CXX20",
-        "_HAS_CXX23"
-    )
     add_rules("@levibuildscript/linkrule")
-    set_exceptions("none")
+    add_rules("@levibuildscript/modpacker")
+    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+    add_defines("NOMINMAX", "UNICODE")
+    add_packages("levilaminalibrary","gmlib")
+    set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
-    set_languages("cxx20")
+    set_languages("c++20")
     set_symbols("debug")
-
-    after_build(function (target)
-        local mod_packer = import("scripts.after_build")
-
-        local mod_define = {
-            modName = target:name(),
-            modFile = path.filename(target:targetfile()),
-        }
-        
-        mod_packer.pack_mod(target,mod_define)
-    end)
+    add_files("src/**.cpp")
+    add_includedirs("src")
+    -- if is_config("target_type", "server") then
+    --     add_includedirs("src-server")
+    --     add_files("src-server/**.cpp")
+    -- else
+    --     add_includedirs("src-client")
+    --     add_files("src-client/**.cpp")
+    -- end
